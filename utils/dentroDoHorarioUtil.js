8debb8bee189dc301dd6148f7f9a875424332f0e
Lumieregyn <lumieregyn.ai@gmail.com>
2025-05-21 16:43:03 -0300

    Update dentroDoHorarioUtil.js

--- utils/dentroDoHorarioUtil.js
+++ utils/dentroDoHorarioUtil.js
@@ -1,6 +1,9 @@
 function dentroDoHorarioUtil(data = new Date()) {
-  const dia = data.getDay(); // 0 = domingo, 6 = sábado
-  const hora = data.getHours();
+  // Ajustar para o fuso de Brasília (GMT-3)
+  const dataBrasil = new Date(data.toLocaleString("en-US", { timeZone: "America/Sao_Paulo" }));
+
+  const dia = dataBrasil.getDay(); // 0 = domingo, 6 = sábado
+  const hora = dataBrasil.getHours();
 
   const diaUtil = dia >= 1 && dia <= 5; // segunda a sexta
   const horarioComercial = hora >= 8 && hora < 19;
